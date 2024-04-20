9d1f406749a7b9d08f818c03bfe2902170be3162
jcfe13 <johncarloesquillo72@gmail.com>
2024-04-21 01:35:58 +0800

    Initializing the variables

--- students_highest_gwa_reader.py
+++ students_highest_gwa_reader.py
@@ -1,3 +1,5 @@
 # 1. Initialize variables to track the student with the highest GWA
+highest_gwa = float('inf')
+top_student = ''
 # 2. Open the file and read all lines
 # 3. Display the student who achieved the highest GWA
