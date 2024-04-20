915bbacd42e1645ac7913b7f3ab3baf142847aae
jcfe13 <johncarloesquillo72@gmail.com>
2024-04-20 20:13:38 +0800

    Inputting the pseudocode to the program

---
+++ separating_even_and_odd_numbers.py
@@ -0,0 +1,5 @@
+# 1. Read integers from "numbers.txt"
+# 2. Separate integers into even and odd lists
+# 3. Write even numbers to "even.txt"
+# 4. Write odd numbers to "odd.txt"
+# 5. Display completion message
