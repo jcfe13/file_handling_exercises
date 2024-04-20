ce616c6a221b127d0f9cfd51c52ad6089cfc3223
jcfe13 <johncarloesquillo72@gmail.com>
2024-04-20 20:14:29 +0800

    Open the file numbers.txt in read mode and read integers

--- separating_even_and_odd_numbers.py
+++ separating_even_and_odd_numbers.py
@@ -1,4 +1,5 @@
 # 1. Read integers from "numbers.txt"
+with open("numbers.txt", "r") as file:
 # 2. Separate integers into even and odd lists
 # 3. Write even numbers to "even.txt"
 # 4. Write odd numbers to "odd.txt"
