2055363edc4f3828774bd28d3f74ff07a5ef9b96
jcfe13 <johncarloesquillo72@gmail.com>
2024-04-20 20:16:24 +0800

    Stripping any extra whitespaces

--- separating_even_and_odd_numbers.py
+++ separating_even_and_odd_numbers.py
@@ -1,5 +1,6 @@
 # 1. Read integers from "numbers.txt"
 with open("numbers.txt", "r") as file:
+    numbers = [int(num.strip()) for num in file.readlines()]
 # 2. Separate integers into even and odd lists
 # 3. Write even numbers to "even.txt"
 # 4. Write odd numbers to "odd.txt"
