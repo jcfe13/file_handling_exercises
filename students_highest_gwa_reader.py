99f6e6425c57be1d679e2ba93541dd0a9729d9ca
jcfe13 <johncarloesquillo72@gmail.com>
2024-04-21 01:44:01 +0800

    Split the line into name and GWA string

--- students_highest_gwa_reader.py
+++ students_highest_gwa_reader.py
@@ -4,4 +4,5 @@ top_student = ''
 # 2. Open the file and read all lines
 with open("students_gwa.txt", "r") as file:
     for line in file:
+        name, gwa_str = line.strip().split(',')
 # 3. Display the student who achieved the highest GWA
