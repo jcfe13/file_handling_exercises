c53ac2039160ac444833553f7f65a8ecc7f29019
jcfe13 <johncarloesquillo72@gmail.com>
2024-04-20 20:18:27 +0800

    Create a list of even numbers by filtering numbers that are divisible by 2

--- separating_even_and_odd_numbers.py
+++ separating_even_and_odd_numbers.py
@@ -2,6 +2,7 @@
 with open("numbers.txt", "r") as file:
     numbers = [int(num.strip()) for num in file.readlines()]
 # 2. Separate integers into even and odd lists
+even_numbers = [num for num in numbers if num % 2 == 0]
 # 3. Write even numbers to "even.txt"
 # 4. Write odd numbers to "odd.txt"
 # 5. Display completion message
