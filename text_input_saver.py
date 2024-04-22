de9743c910d3a472fc2e336e1d2b0630d034ba03
jcfe13 <johncarloesquillo72@gmail.com>
2024-04-22 17:56:15 +0800

    Newline

--- text_input_saver.py
+++ text_input_saver.py
@@ -1,15 +1,19 @@
 # 1. Initialize an empty list for lines of text
 lines = []
+
 # 2. Loop to get input lines from the user
 while True:
     line = input("Enter line: ")
     lines.append(line)
+
     choice = input("Are there more lines? (y/n): ")
     if choice.lower() != 'y':
         break
+
 # 3. Save lines to 'mylife.txt'
 with open("mylife.txt", "w") as file:
     for line in lines:
         file.write(line + "\n")
+        
 # 4. Display a completion message
 print("Lines have been written to 'mylife.txt'.")
